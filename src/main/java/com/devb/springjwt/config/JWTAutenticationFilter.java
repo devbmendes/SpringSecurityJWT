8e3de95e072c750e7af31a357c2b98c61f4436ff
valter correia <baldhuino@gmail.com>
2023-05-29 14:07:12 +0100

    doFilter method

--- src/main/java/com/devb/springjwt/config/JWTAutenticationFilter.java
+++ src/main/java/com/devb/springjwt/config/JWTAutenticationFilter.java
@@ -9,15 +9,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 
 @Component
+@RequiredArgsConstructor
 public class JWTAutenticationFilter extends OncePerRequestFilter {
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, 
+			HttpServletResponse response, 
+			FilterChain filterChain)
 			throws ServletException, IOException {
+		
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
 
+		if(authHeader == null || authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		jwt = authHeader.substring(7);
 	}
 
 }
